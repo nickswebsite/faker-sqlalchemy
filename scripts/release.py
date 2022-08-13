bf85f657f3d6dc15924843a3f069898349d1c6f4
Nick <nick@nickswoodart.com>
2022-08-13 13:44:06 -0400

    Added support to automatically find `pyenv` to release script.

--- scripts/release.py
+++ scripts/release.py
@@ -1,16 +1,63 @@
 import os
+import shutil
 import subprocess
+import sys
 
 BASEDIR = os.path.dirname(
     os.path.dirname(
         os.path.abspath(__file__),
     )
 )
+PYENV_HOME = os.environ.get("PYENV_HOME", None)
+PYTHON_EXECUTABLES = [
+    "python",
+    "python3.7",
+    "python3.8",
+    "python3.9",
+    "python3.10",
+    "make",
+    "tox",
+    "flit",
+]
+
+
+def validate_executables():
+    errors = []
+    for executable in PYTHON_EXECUTABLES:
+        if shutil.which(executable) is None:
+            errors.append(f"No executable named {executable} found on path.")
+    if errors:
+        print("* " + "\n* ".join(errors))
+        sys.exit(1)
+
+
+def setup_pyenv():
+    global PYENV_HOME
+
+    if PYENV_HOME is None:
+        user_home = os.environ.get("HOME", None)
+        if user_home:
+            if os.path.exists(
+                os.path.join(user_home, ".pyenv")
+            ):
+                PYENV_HOME = os.path.join(user_home, ".pyenv")
+
+    if PYENV_HOME is not None:
+        shims_path = os.path.join(
+            PYENV_HOME,
+            "shims",
+        )
+        if os.path.isdir(shims_path) and shims_path not in os.environ.get("PATH", ""):
+            os.environ["PATH"] = f"{shims_path}{os.pathsep}{os.environ['PATH']}"
 
 
 def main():
+    setup_pyenv()
+
+    validate_executables()
+
     subprocess.run(
-        ["python", "scripts/update_readme.py"],
+        [sys.executable, "scripts/update_readme.py"],
         cwd=BASEDIR,
         check=True,
     )
